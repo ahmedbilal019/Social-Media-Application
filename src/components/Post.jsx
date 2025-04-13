3021db4a7305a78ff3d1198c825efb4cfb945d0f
ahmedbilal <ahmed258bilal@gmail.com>
2025-04-13 11:57:37 +0500

    styling post cards and displaying the default post using useReducer function. creating basic structutre for add post and delete post functions.

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-const Post = () => {
+const Post = ({ post }) => {
   return (
-    <div className="card" style={{ width: "18rem" }}>
+    <div className="card post post-card " style={{ width: "18rem" }}>
       <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" className="btn btn-primary">
-          Go somewhere
-        </a>
+        <h5 className="card-title">
+          {post.title}
+          <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            {post.reactions}
+            <span class="visually-hidden">unread messages</span>
+          </span>
+        </h5>
+        <p className="card-text">{post.body}</p>
+        {post.tags.map((tag) => {
+          return <span className="badge text-bg-primary">{tag}</span>;
+        })}
       </div>
     </div>
   );
--- src/components/store/PostListStore.jsx
+++ src/components/store/PostListStore.jsx
@@ -1,22 +1,46 @@
 import { Children, createContext, useReducer } from "react";
 
-const PostList = createContext({
+export const PostList = createContext({
   postList: [],
   addPost: () => {},
   deletePost: () => {},
 });
+// reducer function
+const postListReducer = (currPostList, action) => {
+  return currPostList;
+};
 
-const PostListProvider = ({ Children }) => {
-  const [postList, dispatechPostList] = useReducer();
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFALT_POST_LIST
+  );
   const addPost = () => {};
-
   const deletePost = () => {};
   return (
     <PostList.Provider
       value={{ postList: postList, addPost: addPost, deletePost: deletePost }}
     >
-      {Children}
+      {children}
     </PostList.Provider>
   );
 };
+const DEFALT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Islamabad",
+    body: "Hey frends! i am going to islamabad, for trip",
+    reactions: 4,
+    userId: "user_9",
+    tags: ["Trip", "Pak-Capital", "islamabad"],
+  },
+  {
+    id: "2",
+    title: "Attending the TECH ceminar",
+    body: "Hey frends! i am going to attend the biggest tech ceminar at Lahore",
+    reactions: 10,
+    userId: "user_10",
+    tags: ["TECH ceminar", "Lahore", "IT"],
+  },
+];
 export default PostListProvider;
